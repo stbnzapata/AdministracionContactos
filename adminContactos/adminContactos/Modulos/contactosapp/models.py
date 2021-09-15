39dc157e6a344cfdc586074179e1b8e8736c5a59
stbnzapata <stbnzpt@gmail.com>
2021-09-15 15:57:21 -0500

    creacion carpeta model y modelo usuario contacto creado

---
+++ adminContactos/adminContactos/Modulos/contactosapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Contacto(models.Model):
+    Id = models.AutoField(primary_key=true)
+    Nombre = models.Charfield(max_length=20)
+    Apellido = models.Charfield(max_length=20)
+    Compañia =  models.Charfield(max_length=100)
+    Cargo =  models.Charfield(max_length=20)
+    Email =  models.Charfield(max_length=50)
+    Celular =  models.Charfield(max_length=10, min_length = 10)
+    Direccion =  models.Charfield(max_length= 100)
+    Ciudad =  models.Charfield(max_length= 100)
