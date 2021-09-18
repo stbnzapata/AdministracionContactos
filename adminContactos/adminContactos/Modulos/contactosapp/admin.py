3addcb4ecb44d6c865188c4521bf2db6dbad294d
mao-rodriguez <54720405+mao-rodriguez@users.noreply.github.com>
2021-09-17 22:50:10 -0500

    Se adiciona modelo Empresa. Se instala modulo firebase. Se adiciona archivo .gitignore

--- adminContactos/adminContactos/Modulos/contactosapp/admin.py
+++ adminContactos/adminContactos/Modulos/contactosapp/admin.py
@@ -1,3 +1,6 @@
+from adminContactos.adminContactos.Modulos.contactosapp.models import Contacto, Empresa
 from django.contrib import admin
-
+from Modulos.contactosapp import *
 # Register your models here.
+admin.site.register(Contacto)
+admin.site.register(Empresa)
